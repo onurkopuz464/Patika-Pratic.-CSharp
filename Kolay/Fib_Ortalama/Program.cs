6d5ba0dd8bd0cb9cd503f808b7b9ae2a1f037ae0
onurkopuz464 <onurkopuz464@gmail.com>
2022-11-03 00:28:56 +0300

    1
    1

---
+++ Kolay/Fib_Ortalama/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Sayi Giriniz : ");
+int value = Convert.ToInt32(Console.ReadLine());
+Fib_(value);
+
+
+static void Fib_(int lenght)
+{
+    int x = 0, y = 1, z = 0; double ort = 1;
+
+    Console.Write("{0} {1}", x, y);
+
+    for (int i = 2; i < lenght; i++)
+    {
+        z = x + y;
+        Console.Write(" {0}", z);
+        x = y; y = z;
+        ort += z;
+    }
+
+    Console.WriteLine();
+    
+    Console.WriteLine("Ortalama : "+(ort/lenght));
+}
+
