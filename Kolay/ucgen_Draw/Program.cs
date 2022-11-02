6afa374c2c2f7d008dc56975e402d2415bbfccd0
onurkopuz464 <onurkopuz464@gmail.com>
2022-11-03 01:08:20 +0300

    ucgen_draw
    1

---
+++ Kolay/ucgen_Draw/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+call_ call = new call_();
+Console.Write("Lütfen bir sayı giriniz :");
+int input = Convert.ToInt32(Console.ReadLine());
+call.build(input);
+
+
+public  class call_
+{
+    
+
+    public void build(int x)
+    {
+       
+        for (int i = 0; i <= x; i++)
+        {
+            for (int z = 0; z < (x-i); z++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j < i; j++)
+            {
+               
+                Console.Write("*");
+            }
+            for (int f = 0; f < i; f++)
+            {
+
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+        }
+    }
+
+
+}
