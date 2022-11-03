f4478c7719699b41c22bb7361b2a07c7278d2a49
onurkopuz464 <onurkopuz464@gmail.com>
2022-11-03 17:47:59 +0300

    1
    1

---
+++ Orta/Integer_İkililerin_Toplamı/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("ikilileri girin : ");
+
+string STvalues = Console.ReadLine();
+string[] Arrayvalues = STvalues.Split(' ');
+for (int i = 0; i < Arrayvalues.Length-1; i=+i+2)
+{  
+        int values = Int32.Parse(Arrayvalues[i]), _values = Int32.Parse(Arrayvalues[i + 1]);      
+        if (values != _values)
+        {
+            Console.Write(values + _values + " ");
+        }
+        else
+        {
+            Console.Write(Math.Pow((values + _values), 2) + " ");
+        }   
+}
