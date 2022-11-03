12baa119a08e889548413b0b6743d11b1f3776e6
onurkopuz464 <onurkopuz464@gmail.com>
2022-11-03 15:42:42 +0300

    1
    1

---
+++ Kolay/Algoritma_Harf_Eksiltme/Program.cs
@@ -0,0 +1,12 @@
+﻿string input = Console.ReadLine(); 
+
+string[] words = input.Split(','); 
+char[] WordArray = words[0].ToCharArray();
+int value = Convert.ToInt32(words[1]);
+
+for (int i = 0; i < WordArray.Length; i++)
+{
+    if(i != value)
+    Console.Write(WordArray[i]);
+
+}
