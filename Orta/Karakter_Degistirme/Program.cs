2c4ea8f8ab43c991eb5a130e092aefefb44fd5cb
onurkopuz464 <onurkopuz464@gmail.com>
2022-11-03 23:35:43 +0300

    1
    1

---
+++ Orta/Karakter_Degistirme/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Cümleyi Girin : ");
+string STvalues = Console.ReadLine();
+string[] Arrayvalues = STvalues.Split(' ');
+
+for (int i = 0; i < Arrayvalues.Length; i++)
+{
+    char[] chars = Arrayvalues[i].ToCharArray();
+    char history = chars[0];
+    chars[0] = chars[chars.Length-1]; 
+    chars[chars.Length-1] = history;
+
+    foreach (var item in chars)
+           Console.Write(item);
+
+    Console.Write(" ");
+
+}
