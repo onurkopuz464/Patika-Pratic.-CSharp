8263fc39d8c878cebbd119b15cf8120b488af13d
onurkopuz464 <onurkopuz464@gmail.com>
2022-11-03 16:02:59 +0300

    1
    1

---
+++ Kolay/Algoritma_Ters_Yazim/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+string[] addstring = Console.ReadLine().Split(' ');
+for (int j = 0; j < addstring.Length; j++)
+{
+    for (int i = addstring[j].Length - 1; i >= 0; i--)
+    {
+        Console.Write(addstring[j][i]);
+    }
+    Console.Write(' ');
+
+}
