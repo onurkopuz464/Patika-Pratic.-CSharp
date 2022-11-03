19c63007282711904534ffaa1a727449bd378964
onurkopuz464 <onurkopuz464@gmail.com>
2022-11-03 23:18:02 +0300

    1
    1

---
+++ Orta/Algoritma_Mutlak_Kare/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("sayıları giriniz : ");
+int mainvalue = 67;
+string STvalues = Console.ReadLine();
+string[] Arrayvalues = STvalues.Split(' ');
+int buyuk = 0, kucuk = 0;
+
+for (int i = 0; i < Arrayvalues.Length; i++)
+{
+    int value = Int32.Parse(Arrayvalues[i]);
+    if (mainvalue > value)
+    {
+        kucuk += mainvalue - value;
+
+
+    }
+    else
+    {
+        buyuk = (int)Math.Pow(value - mainvalue, 2);
+
+    }
+
+}
+Console.Write(kucuk + " " + buyuk);
+
